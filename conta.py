c3f079f5ecd4aa68afcafb52ad65fb2a4c514774
Danillo <danillo.ooo@hotmail.com>
2021-07-14 11:59:42 -0300

    Aula 2 Classes e Objeto - Aprendemos sobre as Classes, Objetos, Fuções conntrutura, Endereço e referência de objetos, Atributos de classe e Acesso aos atributos através do objeto

---
+++ conta.py
@@ -0,0 +1,10 @@
+
+
+class Conta:
+
+    def __init__(self, numero, titular, saldo, limite):
+        print('Contruindo objeto...{}'.format(self))
+        self.numero = numero
+        self.titular = titular
+        self.saldo = saldo
+        self.limite = limite
