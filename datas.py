30f0eb16ada02f98d8f0bde12b7b9a81feab7ec8
Danillo <danillo.ooo@hotmail.com>
2021-07-22 10:20:41 -0300

    Deixei o format do print igual o que o professor está dando nas aulas, mas não concegui ainda resolver o problema do erro de syntax quando digito um numero com 0 no inicio, tipo 08.

--- datas.py
+++ datas.py
@@ -7,4 +7,11 @@ class Data:
         self.ano = ano
 
     def formatada(self):
-        print(f'{self.dia:02}/{self.mes:02}/{self.ano:04}')
+        # print(f'{self.dia:02}/{self.mes:02}/{self.ano:04}')
+        print('{0:02d}/{1:02d}/{2:04d}'.format(self.dia, self.mes, self.ano))
+        # Tenho que ver como corrigir o problema de digitar 08 exemplo:
+        # d = Data(26, 08, 1986)
+        # File "<input>", line 1
+        # d = Data(26, 08, 1986)
+        #               ^
+        # SyntaxError: leading zeros in decimal integer literals are not permitted; use an 0o prefix for octal integers
